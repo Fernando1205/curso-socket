bfc53ef242387ffbc6aefe560856c6c143074897
Fernando Lopez <fer68769@gmail.com>
2022-03-02 14:06:57 -0600

    Esqueleto del proyecto

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const server = require('http').Server(app);
+
+const io = require('socket.io')(server);
+
+app.get('/index', (req, res) => {
+    res.status(200).send({
+        message: 'Indice'
+    })
+});
+
+server.listen(port, () => {
+    console.log(`Listening port: ${port}`);
+});

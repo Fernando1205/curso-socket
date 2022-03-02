b2186a45df732ed5309c473e8ce3aa77834590d2
Fernando Lopez <fer68769@gmail.com>
2022-03-02 14:14:23 -0600

    estructura client

--- server/index.js
+++ server/index.js
@@ -8,10 +8,17 @@ const server = require('http').Server(app);
 
 const io = require('socket.io')(server);
 
+// Middleware
+app.use(express.static('client'));
+
 app.get('/index', (req, res) => {
     res.status(200).send({
         message: 'Indice'
     })
+});
+
+io.on('connection', (socket) => {
+    console.log(`El nodo Ip: ${socket.handshake.address} Se ha conectado`);
 });
 
 server.listen(port, () => {
